4a276b088c0dfb665331f5eaa27cf56f498143fa
himafahim <eprahimahmed37@gmail.com>
2023-06-26 06:08:47 -0700

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:a program that prints all possible different combinations
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 48;
+	int b = 48;
+
+	for (; i <= 49+9; i++)
+	{
+		for (; b <= 49+9; b++)
+		{
+			if (i < b && i != b)
+			{
+				putchar(i);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+		return (0);
+}
